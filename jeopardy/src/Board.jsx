e14039964c59cdb16204f68fac2d34c860e3ffed
threehund0 <samuelbaik87@gmail.com>
2023-08-12 13:22:28 -0700

    added api call

--- jeopardy/src/Board.jsx
+++ jeopardy/src/Board.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from 'react'
+import Category from './Category'
+import Clue from './Clue'
+
 const Board = () => {
-	return <div>Board</div>
+	const [categories, setCategories] = useState([])
+
+	useEffect(() => {
+		async function fetchData() {
+			let offset = Math.floor(Math.random() * 28175 + 1)
+
+			// fetch categories from the API
+
+			const categoryURL = `https://jservice.io/api/categories?count=6&offset=${offset}`
+			const categoryResponse = await fetch(categoryURL)
+			const categoryData = await categoryResponse.json()
+
+			// Fetch clues for each category
+			const categoriesWithClues = await Promise.all(
+				categoryData.map(async category => {
+					const clueURL = ``
+					const clueResponse = await fetch(categoryURL)
+					const clueData = await clueResponse.json()
+
+					return {
+						...category,
+						clues: clueData
+					}
+				})
+			)
+
+			// Put categories and clues inside of state variabe
+			setCategories(categoriesWithClues)
+		}
+		fetchData()
+	}, [])
+
+	return (
+		<div className='board'>
+			{categories.map(category => (
+				<Category key={category.id} category={category} />
+			))}
+		</div>
+	)
 }
 
 /***
